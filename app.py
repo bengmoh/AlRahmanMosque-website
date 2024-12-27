6bce2664f16f37f01950946e5123ade6340c58d4
Mohamed Elamine Bengueddache <142161809+bengmoh@users.noreply.github.com>
2024-12-27 09:01:34 -0500

    Implement first new design Rebase new_design_1 branch on main

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from python_scripts.calculations import add_time
+from python_scripts.calculations import add_time, compare_iqama_times
 import csv
 
 app = Flask(__name__)
@@ -21,21 +21,28 @@ with open("data/prayer_times/today_prayer_times.csv") as file:
         today[prayer] = (athan, iqama)
     opening_time = add_time(today["Fajr"][1], -15, round_to_quarter=False)
     closing_time = add_time(today["Isha"][1], 60, round_to_quarter=False)
+    time_changes = compare_iqama_times(
+        "data/prayer_times/today_prayer_times.csv",
+        "data/prayer_times/tomorrow_prayer_times.csv"
+    )
     
 @app.route('/')
 def index():
     return render_template("prayer_times.html", today=today,
                            opening_time=opening_time,
-                           closing_time=closing_time)
+                           closing_time=closing_time,
+                           time_changes=time_changes)
 
 @app.route('/prayer-times')
 def prayer_times():
     return render_template("prayer_times.html", today=today)
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
 @app.route('/donate')
 def donate():
     return render_template("donate.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html",
+                           opening_time=opening_time,
+                           closing_time=closing_time)
